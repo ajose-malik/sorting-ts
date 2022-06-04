dd0db46a992a9a4d4738a9638cf4186cd114a43a
ajosemalik <ajose.malik@gmail.com>
2022-06-04 14:51:15 -0400

    import and add LinkedList to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {Sorter} from './Sorter'
 import {NumbersCollection} from './NumbersCollection'
 import {CharactersCollection} from './CharactersCollection'
+import {LinkedList} from './LinkedList'
 
 // // Number sorter
 // const numbersCollection = new NumbersCollection([10, 3, -5, 50])
@@ -8,8 +9,20 @@ import {CharactersCollection} from './CharactersCollection'
 // sorter.sort()
 // console.log(numbersCollection.data);
 
-// Character sorter
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection)
+// // Character sorter
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection)
+// sorter.sort()
+// console.log(charactersCollection.data);
+
+// Linkedlist sorter
+const linkedList = new LinkedList()
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList)
 sorter.sort()
-console.log(charactersCollection.data);
+console.log(linkedList.print());
+
