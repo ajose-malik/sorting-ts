7183c6e97eb7a71b4e55143c71a94e736498c4b7
ajosemalik <ajose.malik@gmail.com>
2022-06-04 13:57:08 -0400

    implement CharactersCollection

--- build/index.js
+++ build/index.js
@@ -1,8 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 50]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const CharactersCollection_1 = require("./CharactersCollection");
+// // Number sorter
+// const numbersCollection = new NumbersCollection([10, 3, -5, 50])
+// const sorter = new Sorter(numbersCollection)
+// sorter.sort()
+// console.log(numbersCollection.data);
+// Character sorter
+const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
