2f519b0bc48a5ac8ee9f60c1698d220f9e6ee70b
ajosemalik <ajose.malik@gmail.com>
2022-06-04 15:44:56 -0400

    refactor index.js

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,22 @@ import {NumbersCollection} from './NumbersCollection'
 import {CharactersCollection} from './CharactersCollection'
 import {LinkedList} from './LinkedList'
 
-// Number sorter
-const numbersCollection = new NumbersCollection([10, -200, -5, 50])
-numbersCollection.sort()
-console.log(numbersCollection.data);
+// // Number sorter
+// const numbersCollection = new NumbersCollection([10, -200, -5, 50])
+// numbersCollection.sort()
+// console.log(numbersCollection.data);
 
 // // Character sorter
-// const charactersCollection = new CharactersCollection('Xaayb');
-// const sorter = new Sorter(charactersCollection)
-// sorter.sort()
+// const charactersCollection = new CharactersCollection('XaDyb');
+// charactersCollection.sort()
 // console.log(charactersCollection.data);
 
-// // Linkedlist sorter
-// const linkedList = new LinkedList()
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
-
-// const sorter = new Sorter(linkedList)
-// sorter.sort()
-// console.log(linkedList.print());
+// Linkedlist sorter
+const linkedList = new LinkedList()
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort()
+console.log(linkedList.print());
 
