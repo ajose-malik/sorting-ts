98014234efceff20b0f33241da94f57aa8e19354
ajosemalik <ajose.malik@gmail.com>
2022-06-04 15:36:28 -0400

    fix reference error

--- build/index.js
+++ build/index.js
@@ -1,23 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
-const LinkedList_1 = require("./LinkedList");
-// // Number sorter
-// const numbersCollection = new NumbersCollection([10, 3, -5, 50])
-// const sorter = new Sorter(numbersCollection)
-// sorter.sort()
-// console.log(numbersCollection.data);
+const NumbersCollection_1 = require("./NumbersCollection");
+// Number sorter
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, -200, -5, 50]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 // // Character sorter
 // const charactersCollection = new CharactersCollection('Xaayb');
 // const sorter = new Sorter(charactersCollection)
 // sorter.sort()
 // console.log(charactersCollection.data);
-// Linkedlist sorter
-const linkedList = new LinkedList_1.LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
-const sorter = new Sorter_1.Sorter(linkedList);
-sorter.sort();
-console.log(linkedList.print);
+// // Linkedlist sorter
+// const linkedList = new LinkedList()
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+// const sorter = new Sorter(linkedList)
+// sorter.sort()
+// console.log(linkedList.print());
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import {NumbersCollection} from './NumbersCollection'
 import {CharactersCollection} from './CharactersCollection'
 import {LinkedList} from './LinkedList'
 
-// // Number sorter
-// const numbersCollection = new NumbersCollection([10, 3, -5, 50])
-// const sorter = new Sorter(numbersCollection)
-// sorter.sort()
-// console.log(numbersCollection.data);
+// Number sorter
+const numbersCollection = new NumbersCollection([10, -200, -5, 50])
+numbersCollection.sort()
+console.log(numbersCollection.data);
 
 // // Character sorter
 // const charactersCollection = new CharactersCollection('Xaayb');
@@ -15,14 +14,14 @@ import {LinkedList} from './LinkedList'
 // sorter.sort()
 // console.log(charactersCollection.data);
 
-// Linkedlist sorter
-const linkedList = new LinkedList()
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
+// // Linkedlist sorter
+// const linkedList = new LinkedList()
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
 
-const sorter = new Sorter(linkedList)
-sorter.sort()
-console.log(linkedList.print());
+// const sorter = new Sorter(linkedList)
+// sorter.sort()
+// console.log(linkedList.print());
 
